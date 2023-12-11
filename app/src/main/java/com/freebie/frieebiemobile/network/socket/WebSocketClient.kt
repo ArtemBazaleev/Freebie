fde473c663bea159931c29509712720ff61df2ed
Kirill Achramionok <k.achramionok@gmail.com>
2023-12-12 01:20:54 +0200

    socket subscribe

--- app/src/main/java/com/freebie/frieebiemobile/network/socket/WebSocketClient.kt
+++ app/src/main/java/com/freebie/frieebiemobile/network/socket/WebSocketClient.kt
@@ -7,6 +7,7 @@ import io.socket.client.Socket
 import io.socket.client.SocketOptionBuilder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.json.JSONObject
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -34,7 +35,8 @@ class SocketIOClient @Inject constructor(
     }
 
     override suspend fun initWebSocket(): Result<Boolean> = withContext(Dispatchers.IO) {
-        val token = tokenStorage.getAccessToken()
+        var token = tokenStorage.getAccessToken()
+        token = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxIiwiaXNzIjoiZnJlZWJpZS5hdXRoIiwiaWF0IjoxNzAyMzMyMDg0LCJleHAiOjE3MDI5MzY4ODR9.vhBNesFe1kW8ITBv87zJ1H236Ng-G43RLasvEOgmSydDNBfOjeFQzuVwaVTaSC8e2WX-zjZoJIvrYioaDIjbNs_0b7qZ8ucpIQVPeYgyXVvJABp_U052nCbAPs5avQidDlBGKePxoyp1VodXFkQ2zHp6FZwbQqamixGHLh4zEmkvnS6zNuYv3usDqvJLpJfXdYLDBFghouOcmfJLp2sQ2uAd3WPPrzEqSJucckpXyYT6wxHdhDxC_QmM6_fHNbbtLzAFeT9iVkYtxpZNnfAkVrekyISHSA0aa7e3Sdz8iefu3R-MAU5moIgJxs8bhVn_kLKyrvLCbtLdurgNgnwVoA"
         if (token != null) {
             Log.d("SocketIOClient", "createSocket token = $token")
             socket = createSocket(token)
@@ -51,7 +53,13 @@ class SocketIOClient @Inject constructor(
         Log.d("SocketIOClient", "subscribeOnEvents")
         socket.on(Socket.EVENT_CONNECT) {
             Log.d("SocketIOClient", "Connection status = ${socket.connected()}")
-            socket.emit("subscribe", SubscribeEvent("coupon_purchase", mapOf()))
+            val jsonObject = JSONObject()
+            jsonObject.put("subscribeTo", "coupon_purchase")
+//            val params = JSONObject()  // то что в комменте в случае когда реально нужно отправлять параметры а так без них работает
+//            params.put("1", "1")
+//            params.put("2", "2")
+//            jsonObject.put("params", params)
+            socket.emit("subscribe", jsonObject)
         }
 
 
